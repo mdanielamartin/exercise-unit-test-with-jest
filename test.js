a6516977ad4c589c469b7b68f54992847303aba8
Maria Daniela Martin <m.danielamartin@proton.me>
2024-11-30 03:32:43 +0000

    corrections

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ test("One euro should be 1.07 dollars", function() {
     const { fromEuroToDollar } = require('./app.js');
     const dollars = fromEuroToDollar(3.5);
     const expected = 3.5 * 1.07; 
-    expect(fromEuroToDollar(3.5)).toBe(expected);
+    expect(dollars).toBe(expected);
 })
 
 test("Convert 3.5 Dollar to Yen result: 511.9159", function() {
@@ -19,7 +19,7 @@ test("Convert 3.5 Dollar to Yen result: 511.9159", function() {
     expect(yen).toBe(expected);
 })
 
-test("Convert 3.5 Yen to Pound result: 0.1946", function() {
+test("Convert 3.5 Yen to Pound result: 0.01946", function() {
     const { fromYenToPound } = require('./app.js');
     const pounds = fromYenToPound (3.5);
     const expected = (3.5/156.5)*0.87; 
